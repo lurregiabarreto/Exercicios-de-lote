fc963bc42113f68249d58c2089980735efd6854c
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-08-20 16:15:31 -0300

    clubes da serie A

--- Exercicio 1.bat
+++ Exercicio 1.bat
@@ -5,3 +5,5 @@ md "Serie A"
 md "Serie B"
 
 cd "Serie A"
+
+md "america-mg" "athletico" "atletico-go" "atletico-mh" "bahia" "bragantino" "ceara" "chapecoense" "corinthians" "cuiaba" "fortaleza" "flamengo" "fluminense" "gremio" "internacional" "juventude" "palmeiras" "santos" "sao paulo" "sport"
