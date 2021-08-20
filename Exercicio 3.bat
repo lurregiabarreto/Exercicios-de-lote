7822cab24144249661ccd3a99de29b4b7187177e
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-08-20 17:06:16 -0300

    estados do Brasil

--- Exercicio 3.bat
+++ Exercicio 3.bat
@@ -2,3 +2,7 @@ md "Todos os paises"
 cd "Todos os paises"
 
 md "Argentina" "Bolívia" "Brasil" "Chile" "Colômbia" "Equador" "Guiana" "França" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"
+
+cd ..
+cd "Brasil"
+md "Acre" "alagoas" "amapa" "amazonas" "bahia" "ceara" "espirito santo" "goias" "maranhao" "mato grosso" "mato grosso do sul" "minas gerais" "para" "paraiba" "parana" "pernambuco" "piaui" "Rio de Janeiro" "Rio Grande do Norte" "Rio Grande do Sul" "Rondônia" "Roraima" "Santa Catarina" "São Paulo" "Sergipe" "Tocantins" "Distrito Federal"
