fa55862252ecd47e1c3b3cbdbd67feeb15ef2148
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-08-20 16:21:05 -0300

    clubes da serie b

--- Exercicio 1.bat
+++ Exercicio 1.bat
@@ -7,3 +7,7 @@ md "Serie B"
 cd "Serie A"
 
 md "america-mg" "athletico" "atletico-go" "atletico-mh" "bahia" "bragantino" "ceara" "chapecoense" "corinthians" "cuiaba" "fortaleza" "flamengo" "fluminense" "gremio" "internacional" "juventude" "palmeiras" "santos" "sao paulo" "sport"
+
+cd ..
+cd "Serie B"
+md "Coritiba" "Goiás" "Avaí" "CRB" "Sampaio Corrêa" "Guarani" "Náutico" "Botafogo" "Operário-PR" "CSA" "Vasco" "Remo" "Brusque" "Cruzeiro" "Vila Nova" "Ponte Preta" "Londrina" "Vitória" "Confiança" "Brasil de Pelotas"
