52358bf66312ad9123cb95e91363237cadf77ed9
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-08-20 16:39:28 -0300

    Estados do centro-oeste

--- Exercicio 2.bat
+++ Exercicio 2.bat
@@ -2,3 +2,5 @@ md "Todos os estados"
 cd "Todos os estados"
 
 md "Centro-oeste" "Nordeste" "Sul" "Suldeste" "Norte"
+cd "Centro-oeste"
+md "Distrito Federal" "Goias" "Mato Grosso" "Mato Grosso do Sul"
