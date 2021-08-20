85575d1225f168d701517b03bddd015d2e977311
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-08-20 16:41:07 -0300

    estados do nordeste

--- Exercicio 2.bat
+++ Exercicio 2.bat
@@ -4,3 +4,7 @@ cd "Todos os estados"
 md "Centro-oeste" "Nordeste" "Sul" "Suldeste" "Norte"
 cd "Centro-oeste"
 md "Distrito Federal" "Goias" "Mato Grosso" "Mato Grosso do Sul"
+
+cd ..
+cd "Nordeste"
+md "Alagoas" "Bahia" "Ceara" "Maranhao" "Paraiba" "Pernambuco" "Piaui" "Rio Grande do Norte" "Sergipe"
